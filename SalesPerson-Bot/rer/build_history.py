f6e943ed9827e418825acf19091b557ba56cebb5
sanjanaramprasad <sanjanaramprasad1@gmail.com>
2016-02-02 00:45:07 +0530

    Create build_history.py

---
+++ SalesPerson-Bot/rer/build_history.py
@@ -0,0 +1,49 @@
+def build_history(data_list, supported_tags):
+    history_list = [] # list of all histories
+    sents = []
+    count = 0
+    expected = []
+    for data in data_list: # data is the inputs entered by a given student
+        data1 = data['data']
+        for rec in data1:
+            updates = rec['updates']
+            sent = rec['sentence']
+            words = []
+#            print rec
+            rels = rec.get("rels",{"irrelevant":[]})
+            expected.append(rels)
+            
+            for i in range(len(updates)):
+                words.append(updates[i]['word'])
+                #------------------------------------------------------------------------------------------------
+                # NOTE: below code is a temporary hack to build the MAxEnt for just 2 tags - we will change this later
+                if (updates[i]['tag'] not in supported_tags):
+                    if updates[i]['tag'] == "Model":
+                        updates[i]['tag'] = "Version"
+                    else:
+                        updates[i]['tag'] = "Other"                
+                #------------------------------------------------------------------------------------------------
+            sents.append(words)            
+            '''for i in range(len(updates)):
+                history = {}
+                history["i"] = i
+                if i == 0:
+                    history["ta"] = "*" # special tag
+                    history["tb"] = "*" # special tag
+                elif i == 1:
+                    history["ta"] = "*" # special tag
+                    history["tb"] = updates[i - 1]['tag']
+                else:
+                    history["ta"] = updates[i - 2]['tag'] 
+                    history["tb"] = updates[i - 1]['tag']
+                history["wn"] = count
+                history_list.append((history, updates[i]['tag'], ))'''
+            history = {}
+            history["i"] = i
+            history["updates"] = updates
+            history["sentences"] = words
+            history["wn"] = count
+            #history["rels"] = rels
+            history_list.append((history, rels,))			
+            count += 1
+    return (history_list, sents, expected)
